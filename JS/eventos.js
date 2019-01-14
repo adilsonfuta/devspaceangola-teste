69ac22a0eed8c06911af1b61656e2207c384fb4a
adilsonfuta <adilsonmariafuta@gmail.com>
2019-01-15 00:29:43 +0100

    ja antes de enviar online

---
+++ JS/eventos.js
@@ -0,0 +1,17 @@
+
+
+
+function SOMA(a,b){
+        return a+b;
+}
+
+
+var action=DocumentType.call;
+
+var a=Console("testando as senas "+a);
+
+function somatorio(a){
+if(a==0)
+return 0;
+return a+somatorio(a-1);
+}
